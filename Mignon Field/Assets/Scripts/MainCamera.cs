829a2332cae347b9299c50ffc59ee07cdc4b328f
jljl73 <jljl73@naver.com>
2020-11-10 00:47:24 +0900

    msg

---
+++ Mignon Field/Assets/Scripts/MainCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 카메라 위치를 Player의 2, 3, -2 만큼 떨어진 곳으로 고정
+        transform.position = Player.transform.position + new Vector3(2, 3, -2);
+    }
+}
---
+++ Mignon Field/Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public float Moving_Speed;
+    private bool is_running = false;
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Animator>().SetBool("Is_Running", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        is_running = false;
+        if (Input.GetKey(KeyCode.W))
+        {
+            is_running = true;
+            transform.rotation = Quaternion.Euler(0, 305, 0);
+            transform.position += new Vector3(-1, 0, 1f) * Time.deltaTime * Moving_Speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            is_running = true;
+            transform.rotation = Quaternion.Euler(0, 215, 0);
+            transform.position += new Vector3(-1f, 0, -1f) * Time.deltaTime * Moving_Speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            is_running = true;
+            transform.rotation = Quaternion.Euler(0, 135, 0);
+            transform.position += new Vector3(1f, 0, -1f) * Time.deltaTime * Moving_Speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            is_running = true;
+            transform.rotation = Quaternion.Euler(0, 45, 0);
+            transform.position += new Vector3(1, 0, 1f) * Time.deltaTime * Moving_Speed;
+        }
+
+        if (is_running)
+        {
+            GetComponent<Animator>().SetBool("Is_Running", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Is_Running", false);
+        }
+    }
+}
