502a0090ca272864856d6028fc0022fdc95f074b
kjy276 <kjy276@naver.com>
2020-11-25 12:11:57 +0900

    Apply Raycast Layer

--- Mignon Field/Assets/Scripts/MainCamera.cs
+++ Mignon Field/Assets/Scripts/MainCamera.cs
@@ -2,21 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MainCamera : MonoBehaviour
 {
     public Player Player;
     public Camera Camera;
     private RaycastHit hit;
     // Start is called before the first frame update
+    
+
+
     void Start()
     {
+
         try
         {
             if (Player.PV.IsMine)
             {
                 transform.gameObject.SetActive(true);
             }
-            int layerMaks = 1 << LayerMask.NameToLayer("buildings");
+
         }
         catch
         {
@@ -28,6 +33,7 @@ public class MainCamera : MonoBehaviour
     // Update is called once per frame
     void Update() 
     {
+
         // 카메라 위치를 Player의 일정거리만큼 떨어진 곳으로 고정
         try
         {
@@ -37,14 +43,15 @@ public class MainCamera : MonoBehaviour
         catch
         {
         }
-
-        if (Physics.Raycast(transform.position, transform.forward, out hit)){
+        //LayerMask mask = LayerMask.GetMask("Buildings");
+        int mask = 1 << 9;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, mask)){
             Debug.Log("hit point : " + hit.point + ", distance : " + hit.distance + ", name : " + hit.collider.name); 
             Debug.DrawRay(transform.position, transform.forward * hit.distance, Color.red);
         }
 
         RaycastHit[] hits;
-        hits = Physics.RaycastAll(transform.position, transform.forward, 100.0f);
+        hits = Physics.RaycastAll(transform.position, transform.forward, 15.0f, mask);
 
         for(int i=0;i<hits.Length;i++){
             RaycastHit hit = hits[i];
